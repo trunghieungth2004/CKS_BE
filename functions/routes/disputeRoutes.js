316f5c6fa1bccaf4b2d71d6a42f0ff1e64caa535
Nguyen Trung Hieu <hieuntse184626@fpt.edu.vn>
2026-03-01 17:07:45 +0700

    modify dispute endpoints && modify inventory endpoints

--- functions/routes/disputeRoutes.js
+++ functions/routes/disputeRoutes.js
@@ -5,14 +5,14 @@ const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 const { strictLimiter } = require('../middleware/securityMiddleware');
 
-router.post('/dispute', authMiddleware, roleMiddleware([4]), strictLimiter, disputeController.fileDispute);
+router.post('/', authMiddleware, roleMiddleware([4]), strictLimiter, disputeController.fileDispute);
 
-router.post('/disputes/order', authMiddleware, roleMiddleware([0, 3, 4]), disputeController.getDisputesByOrder);
+router.post('/order', authMiddleware, roleMiddleware([0, 3, 4]), disputeController.getDisputesByOrder);
 
-router.post('/disputes/all', authMiddleware, roleMiddleware([0, 3]), disputeController.getAllDisputes);
+router.post('/all', authMiddleware, roleMiddleware([0, 3]), disputeController.getAllDisputes);
 
-router.post('/disputes/my-disputes', authMiddleware, roleMiddleware([4]), disputeController.getMyDisputes);
+router.post('/my-disputes', authMiddleware, roleMiddleware([4]), disputeController.getMyDisputes);
 
-router.post('/dispute/resolve', authMiddleware, roleMiddleware([0, 3]), strictLimiter, disputeController.resolveDispute);
+router.post('/resolve', authMiddleware, roleMiddleware([0, 3]), strictLimiter, disputeController.resolveDispute);
 
 module.exports = router;
--- functions/routes/inventoryRoutes.js
+++ functions/routes/inventoryRoutes.js
@@ -4,8 +4,8 @@ const inventoryController = require('../controllers/inventoryController');
 const authMiddleware = require('../middleware/authMiddleware');
 const roleMiddleware = require('../middleware/roleMiddleware');
 
-router.get('/store', authMiddleware, roleMiddleware([4]), inventoryController.getStoreInventory);
+router.post('/store', authMiddleware, roleMiddleware([4]), inventoryController.getStoreInventory);
 
-router.get('/ck', authMiddleware, roleMiddleware([1, 3]), inventoryController.getCKInventory);
+router.post('/ck', authMiddleware, roleMiddleware([1, 3]), inventoryController.getCKInventory);
 
 module.exports = router;
